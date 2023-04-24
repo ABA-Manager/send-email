34ddfc49094a527a6447b067e6154af7ac4f69cb
rayyero <raydelyero96@gmail.com>
2023-04-24 15:56:28 -0400

    update(url register)

--- Setting.py
+++ Setting.py
@@ -2,10 +2,10 @@
 #variables para config y BDconection
 
 MAIL_USERNAME="raytestmails@gmail.com"
-MAIL_PASSWORD="hPq2JQTNVaD0Bz3f"
+MAIL_PASSWORD="qucnwgfjchxysinr"
 MAIL_FROM="raytestmails@gmail.com"
 MAIL_PORT=587
-MAIL_SERVER="smtp-relay.sendinblue.com"
+MAIL_SERVER="smtp.gmail.com"
 DB_HOST="lin-13704-4133-pgsql-primary.servers.linodedb.net"
 DB_NAME="aba_test"
 DB_USER="linpostgres"
