a9c5305d1bd950ac4c0d5902bba2ded4f72c7b32
rayyero <raydelyero96@gmail.com>
2023-04-25 21:04:05 -0400

    update(url register)

--- Setting.py
+++ Setting.py
@@ -1,9 +1,9 @@
 
 #variables para config y BDconection
 
-MAIL_USERNAME="raytestmails@gmail.com"
-MAIL_PASSWORD="qucnwgfjchxysinr"
-MAIL_FROM="raytestmails@gmail.com"
+MAIL_USERNAME="raydelyero96@gmail.com"
+MAIL_PASSWORD="sprfriknyvuudfte"
+MAIL_FROM="raydelyero96@gmail.com"
 MAIL_PORT=587
 MAIL_SERVER="smtp.gmail.com"
 DB_HOST="lin-13704-4133-pgsql-primary.servers.linodedb.net"
--- main.py
+++ main.py
@@ -38,7 +38,7 @@ async def send_email(contractor_id: int):
     #Cambiar con la direccion del servidor
     link = f"Setting.URL_COMPANY/register/{contractor_id}"
 
-    with open("/app/Templates/email_template.html", "r",encoding='utf-8') as f:
+    with open("Templates/email_template.html", "r",encoding='utf-8') as f:
         template = f.read()
 
     body = template.replace('{{ name }}', name).replace('{{ link }}',link )
